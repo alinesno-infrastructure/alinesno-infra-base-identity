e5c72f603c219bef33eb6cc6b4675ef2294e7b27
switchII <luoandon@gmail.com>
2023-09-26 22:06:08 +0800

    fix:处理编码的问题

--- alinesno-infra-base-identity-auth-application/src/main/java/com/alinesno/infra/base/identity/auth/controller/CaptchaController.java
+++ alinesno-infra-base-identity-auth-application/src/main/java/com/alinesno/infra/base/identity/auth/controller/CaptchaController.java
@@ -1,7 +1,6 @@
 package com.alinesno.infra.base.identity.auth.controller;
 
 import cn.hutool.core.codec.Base64;
-import com.alinesno.infra.common.core.constants.CacheConstants;
 import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.utils.IdUtils;
 import com.google.code.kaptcha.Producer;
@@ -18,12 +17,7 @@ import java.io.IOException;
  * 生成验证二维码
  */
 @RestController
-public class CommonCaptchaController {
-
-    /**
-     * 验证码有效期（分钟）
-     */
-    public static final Integer CAPTCHA_EXPIRATION = 2;
+public class CaptchaController {
 
     @Resource(name = "captchaProducer")
     private Producer captchaProducer;
@@ -31,41 +25,40 @@ public class CommonCaptchaController {
     @Resource(name = "captchaProducerMath")
     private Producer captchaProducerMath;
 
+    public CaptchaController(Producer captchaProducer, Producer captchaProducerMath) {
+        this.captchaProducer = captchaProducer;
+        this.captchaProducerMath = captchaProducerMath;
+    }
+
     /**
      * 生成验证码
      */
     @GetMapping("/captchaImage")
-    public AjaxResult getCode() throws IOException
-    {
+    public AjaxResult captchaImage() throws IOException {
         AjaxResult ajax = AjaxResult.success();
 
         // 保存验证码信息
         String uuid = IdUtils.simpleUUID();
-        String verifyKey = CacheConstants.CAPTCHA_CODE_KEY + uuid;
 
-        String capStr = null, code = null;
-        BufferedImage image = null;
+        String capStr;
+        BufferedImage image;
 
         // 生成验证码
-        String captchaType = "math" ;
         String capText = captchaProducerMath.createText();
         capStr = capText.substring(0, capText.lastIndexOf("@"));
-        code = capText.substring(capText.lastIndexOf("@") + 1);
         image = captchaProducerMath.createImage(capStr);
 
         // 转换流信息写出
         FastByteArrayOutputStream os = new FastByteArrayOutputStream();
-        try
-        {
+        try {
             ImageIO.write(image, "jpg", os);
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             return AjaxResult.error(e.getMessage());
         }
 
         ajax.put("uuid", uuid);
         ajax.put("img", Base64.encode(os.toByteArray()));
+
         return ajax;
     }
 }
