8d49f206b0b3634b96359fd0defd06d45604ea40
switchII <luoandon@gmail.com>
2024-02-15 16:10:11 +0800

    feat:去掉多余的类

--- alinesno-infra-base-identity-auth-application/src/main/java/com/alinesno/infra/base/identity/auth/IdentityAuthApplication.java
+++ alinesno-infra-base-identity-auth-application/src/main/java/com/alinesno/infra/base/identity/auth/IdentityAuthApplication.java
@@ -1,13 +1,9 @@
 package com.alinesno.infra.base.identity.auth;
 
-import com.alinesno.infra.base.authority.gateway.dto.ManagerAccountDto;
-import com.alinesno.infra.base.identity.adapter.ManagerAccountConsumer;
 import com.alinesno.infra.common.core.auto.EnableCore;
-import com.alinesno.infra.common.web.adapter.utils.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * 集成一个Java开发示例工具
